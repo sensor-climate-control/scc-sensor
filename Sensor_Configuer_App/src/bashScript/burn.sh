f2e5156d1316260ba44f627c76208d574bd93e39
Marcelo Salas <salasma@oregonstate.edu>
2023-05-09 14:07:15 -0700

    test fix of burn.sh no arduino-cli found

--- Sensor_Configuer_App/src/bashScript/burn.sh
+++ Sensor_Configuer_App/src/bashScript/burn.sh
@@ -3,17 +3,17 @@ echo "Downloading arduino-cli"
 echo "$(dirname $(dirname "$0"))"
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=~/Downloads/sensor_Configurer sh
 echo "Installing Libraries: "
-../arduino-cli.exe lib install "Adafruit Unified Sensor"
-../arduino-cli.exe lib install "DHT sensor library"
-../arduino-cli.exe lib install "WiFiNINA"
-../arduino-cli.exe lib install "ArduinoMqttClient"
-../arduino-cli.exe lib install "Ethernet_Generic"
-../arduino-cli.exe lib install "ArduinoJson"
-../arduino-cli.exe lib install "ArduinoHttpClient"
+../arduino-cli lib install "Adafruit Unified Sensor"
+../arduino-cli lib install "DHT sensor library"
+../arduino-cli lib install "WiFiNINA"
+../arduino-cli lib install "ArduinoMqttClient"
+../arduino-cli lib install "Ethernet_Generic"
+../arduino-cli lib install "ArduinoJson"
+../arduino-cli lib install "ArduinoHttpClient"
 echo ""
 
 echo "Installing Board Support: "
-../arduino-cli.exe core install "arduino:mbed_nano"
+../arduino-cli core install "arduino:mbed_nano"
 echo ""
 
 echo "Getting Location: "
@@ -22,8 +22,8 @@ current_dir=${PWD##*/}
 #echo "$current_dir.ino"
 
 echo "Compiling Code (may take 30+ seconds): "
-../arduino-cli.exe compile ./$current_dir.ino -b arduino:mbed_nano:nanorp2040connect
-../arduino-cli.exe board list
+../arduino-cli compile ./$current_dir.ino -b arduino:mbed_nano:nanorp2040connect
+../arduino-cli board list
 echo ""
 
 echo "Enter the port where nanorp2040connect is located: "
@@ -31,5 +31,5 @@ read portnum
 echo ""
 
 echo "Loading code on Sensor Module: "
-../arduino-cli.exe upload ./$current_dir.ino -b "arduino:mbed_nano:nanorp2040connect" -p $portnum
+../arduino-cli upload ./$current_dir.ino -b "arduino:mbed_nano:nanorp2040connect" -p $portnum
 echo "Success!"
